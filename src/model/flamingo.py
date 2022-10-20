619eb0f7899e4ebc0912458686e58d8949ed13e8
Anas Awadalla <anasa2@uw.edu>
2022-10-20 11:51:25 -0700

    fixed typos and naming in model

--- src/model/flamingo.py
+++ src/model/flamingo.py
@@ -1,36 +1,37 @@
 '''
-Main Flamingo class
+Flamingo model
 Uses gated cross attention with Perceiver resampler
 '''
 
 from helpers import GatedCrossAttentionBlock, PerceiverResampler
+from torch import nn
 
 class Flamingo(nn.Module):
   '''
     parameters:
       vision_encoder: any vision encoder
-      lang_encoder: ModuleList containing language encoder blocks
+      lang_decoder: ModuleList containing language decoder blocks
   '''
-  def __init__(self, vision_encoder, lang_encoder):
+  def __init__(self, vision_encoder, lang_decoder):
     self.vision_encoder = vision_encoder
-    self.lang_encoder = lang_encoder
+    self.lang_decoder = lang_decoder
     
     self.gated_cross_attn = nn.ModuleList([
       GatedCrossAttentionBlock(dim)
-      for _ in range(len(lang_encoder))
+      for _ in range(len(self.lang_decoder))
     ])
     
     self.perceiver_resampler = nn.ModuleList([
       PerceiverResampler(dim)
-      for _ in range(len(lang_encoder))
+      for _ in range(len(self.lang_decoder))
     ])
 
-  def forward(self, vision_x, lang_x):
-    vision_attended = self.vision_encoder(vision_x)
+  def forward(self, vision_input, text_input):
+    vision_attended = self.vision_encoder(vision_input)
     
-    for i in range(len(self.lang_encoder)):
-      vision_x = self.perceiver_resampler[i](vision_attended)
-      lang_x = self.gated_cross_attn[i](lang_x, vision_x)
-      lang_x = self.language_encoder[i](lang_x)
+    for i in range(len(self.lang_decoder)):
+      vision_input = self.perceiver_resampler[i](vision_attended)
+      text_input = self.gated_cross_attn[i](text_input, vision_input)
+      text_input = self.lang_decoder[i](text_input)
    
-   return lang_x
+    return text_input
