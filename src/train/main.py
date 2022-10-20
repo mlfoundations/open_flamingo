a10c1b3cfccce255d8f6c12ff88e8b13fcd4f8dc
riotonzuk <57375692+riotonzuk@users.noreply.github.com>
2022-10-19 19:51:19 -0700

    get started with training pipeline, add stub

---
+++ src/train/main.py
@@ -0,0 +1,7 @@
+''' Main training script '''
+
+from torch.utils.data import DataLoader, Dataset
+from torchvision.transforms import Compose, RandomHorizontalFlip, ColorJitter
+
+def train(...):
+  ...
