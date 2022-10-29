9c542c23b6e7980721815c6e3e6f95e6a5a477fd
riotonzuk <57375692+riotonzuk@users.noreply.github.com>
2022-10-29 00:38:08 -0700

    draft train pipeline
    a template of what's to come

--- src/train/main.py
+++ src/train/main.py
@@ -3,5 +3,39 @@
 from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import Compose, RandomHorizontalFlip, ColorJitter
 
-def train():
-  raise NotImplementedError
+class ImageTextDataset(Dataset):
+  pass
+
+def train(data_path, **data_kwargs):
+  # load data from tars
+  train_dataset = ImageTextDataset(data_path, train=True)
+  eval_dataset = ImageTextDataset(data_path, train=False)
+  
+  # load DataLoaders
+  train_loader = DataLoader(train_dataset, **data_kwargs)
+  eval_loader = DataLoader(eval_dataset, **data_kwargs)
+  
+  # load model
+  model = Flamingo(...)
+  
+  # train model
+  for epoch in range(num_epochs):
+    for batch in train_loader:
+      optim.zero_grad()
+      inputs, labels = [v.to(device) for v in batch]
+      preds = model(inputs)
+      loss = loss_fn(preds, labels)
+      loss.backward()
+      optim.step()
+     
+    # evaluate model on zero-shot ImageNet
+    with torch.no_grad():
+      total_correct = 0
+      
+      for batch in eval_loader:
+        optim.zero_grad()
+        inputs, labels = [v.to(device) for v in batch]
+        preds = model(inputs)
+        total_correct += (preds.argmax(...) == labels).sum()
+      
+      print(f"acc @{epoch}: {total_correct / len(eval_dataset)}")
