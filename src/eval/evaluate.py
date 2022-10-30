be9603afe1794efd682f8828131ba423015b9ffe
Anas Awadalla <anasa2@uw.edu>
2022-10-30 16:15:27 -0700

    Added 0-shot imagenet eval

--- src/eval/evaluate.py
+++ src/eval/evaluate.py
@@ -10,6 +10,83 @@ import re
 
 from .utils import compute_vqa_accuracy
 
+from .image_net_zeroshot import imagenet_classnames
+
+
+def evaluate_imagenet_zeroshot(model, tokenizer, image_processor, batch_size, num_samples=None, num_classes=10, device=-1):
+    """Evaluate a model on ImageNet. Need to set a auth_token using huggingface-cli login to use the dataset.
+
+    Args:
+        model (nn.Module): model to evaluate
+        tokenizer (transformers.PreTrainedTokenizer): tokenizer for the model
+        image_processor (transformers.ImageProcessor): image processor for the model
+        batch_size (int): batch size
+        num_samples (int, optional): number of samples to evaluate on (for testing). Defaults to None (entire testset).
+        num_classes (int, optional): number of classes to evaluate on (for testing).
+        device (int, optional): device to use. Defaults to -1 (cpu).
+
+    Returns:
+        dict: dictionary of metrics
+    """
+    logging.info("Evaluating on ImageNet zero-shot...")
+
+    if num_classes > 1000:
+        raise ValueError("num_classes must be <= 1000")
+
+    dataset = load_dataset("imagenet-1k", split="validation", streaming=True, use_auth_token=True)
+
+    # if num_samples is not None:
+    #     dataset = dataset.shuffle().select(range(num_samples))
+    # dataset = dataset.map(lambda x: {"image": image_processor(x[0])["pixel_values"], "label": x[1]}, batched=True)
+
+    bos_token = tokenizer.bos_token
+    eoc_token = "<|endofchunk|>"
+
+    imagenet_class_prompts = [f"<image> A photo of an {label} {eoc_token}" if label[0] in "aeiou" else f"<image> A photo of a {label} {eoc_token}" for label in imagenet_classnames[:num_classes]]
+    imagenet_class_inputs = tokenizer(imagenet_class_prompts, padding="max_length", return_tensors="pt", max_length=20)
+
+    model.eval()
+    model.to(device if device >= 0 else "cpu")
+
+    predictions = []
+
+    with torch.inference_mode():
+        for row in tqdm(dataset):
+            image = image_processor(images=[row["image"]], return_tensors="pt")["pixel_values"]
+            label = row["label"]
+
+            per_class_loss = []
+
+            # batched forward pass on imagenet_class_inputs
+            for i in tqdm(range(0, len(imagenet_class_inputs["input_ids"]), batch_size)):
+                batch = {k: v[i:i+batch_size].to(device if device >= 0 else "cpu") for k, v in imagenet_class_inputs.items()}
+                # repeat image batch_size times
+                images_inputs = image.repeat(min(batch_size, len(imagenet_class_inputs["input_ids"]) - i), 1, 1, 1)
+                logits = model(images_inputs, batch["input_ids"], attention_mask=batch["attention_mask"])[0]
+                labels = batch["input_ids"].clone()
+                # convert padding tokens to -100 so they are ignored in loss
+                labels[labels == tokenizer.pad_token_id] = -100
+
+                # Loss computation from OPT code:
+                # Compute per instance loss
+                # Shift so that tokens < n predict n
+                shift_logits = logits[..., :-1, :].contiguous()
+                shift_labels = labels[..., 1:].contiguous()
+                loss_fct = CrossEntropyLoss(reduction="sum")
+                # Flatten the tokens
+                loss = loss_fct(
+                    shift_logits.view(-1, model.lang_encoder.config.vocab_size), shift_labels.view(-1))
+                loss = [loss_instance.item() for loss_instance, tokenize_input in zip([loss], tokenizer.batch_decode(batch["input_ids"], skip_special_tokens=True))]
+                per_class_loss.extend(loss)
+
+            predictions.append(np.argmin(per_class_loss))
+
+            if len(predictions) == num_samples:
+                break
+
+    print("accuracy", (predictions == label).mean())
+    return {"accuracy": (predictions == label).mean()}
+
 def evaluate_text_vqa(model, tokenizer, image_processor, batch_size, max_generation_length=30, num_samples=None, device=-1):
     """Evaluate a model on TextVQA.
 
