e3dd2e584dd321fcc447100760d030b4247896c9
Anas Awadalla <anasa2@uw.edu>
2022-10-31 10:35:18 -0700

    fixed typo in vqa test

--- tests/test_evaluations.py
+++ tests/test_evaluations.py
@@ -5,7 +5,7 @@ from src.open_flamingo.factory import create_model_and_transforms
 
 
 class TestEvaluations(unittest.TestCase):
-    def test_zeroshot_imagenet(self):
+    def test_zeroshot_text_vqa(self):
         model, image_processor, tokenizer = create_model_and_transforms(
             "openai/clip-vit-base-patch32", "facebook/opt-125m")
 
