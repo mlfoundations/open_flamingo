d9127898aad4d133047d66a3636aa579ba160406
Mitchell Wortsman <mitchellw@ip-172-31-38-204.ec2.internal>
2022-11-27 21:51:06 +0000

    low prec training

--- src/train_utils.py
+++ src/train_utils.py
@@ -1,14 +1,37 @@
+from contextlib import suppress
 from tqdm import tqdm
 import torch
 
-def train_one_epoch(args, model, train_loader, tokenizer, optimizer, lr_scheduler, device_id, wandb):
+def get_cast_dtype(precision: str):
+    cast_dtype = None
+    if precision == 'bf16':
+        cast_dtype = torch.bfloat16
+    elif precision == 'fp16':
+        cast_dtype = torch.float16
+    return cast_dtype
+
+def get_autocast(precision):
+    if precision == 'amp':
+        return torch.cuda.amp.autocast
+    elif precision == 'amp_bfloat16' or precision == 'amp_bf16':
+        # amp_bfloat16 is more stable than amp float16 for clip training
+        return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
+    else:
+        return suppress
+
+def train_one_epoch(args, model, epoch, train_loader, tokenizer, optimizer, lr_scheduler, device_id, wandb):
+    num_batches_per_epoch = train_loader.num_batches
+    autocast = get_autocast(args.precision)
+    cast_dtype = get_cast_dtype(args.precision)
+
     model.train()
-    for num_steps, batch in tqdm(enumerate(train_loader), disable=args.rank != 0):      
-        images = batch[0].to(device_id)
+    for num_steps, batch in tqdm(enumerate(train_loader), disable=args.rank != 0):
+        global_step = num_steps + epoch * num_batches_per_epoch  
+        images = batch[0].to(device_id, dtype=cast_dtype, non_blocking=True)
         
         # get all zero index elements from pairs in batch[1]
-        input_ids = batch[1][0].to(device_id)
-        attention_mask = batch[1][1].to(device_id)
+        input_ids = batch[1][0].to(device_id, non_blocking=True)
+        attention_mask = batch[1][1].to(device_id, dtype=cast_dtype, non_blocking=True)
         labels = input_ids.clone()
         
         # Do not compute loss on padding tokens
@@ -17,8 +40,8 @@ def train_one_epoch(args, model, train_loader, tokenizer, optimizer, lr_schedule
         labels[:, 0] = -100
         labels[:, 1] = -100
         labels.to(device_id)
-        
-        loss = model(images, input_ids, attention_mask=attention_mask, labels=labels)[0]
+        with autocast():
+            loss = model(images, input_ids, attention_mask=attention_mask, labels=labels)[0]
         divided_loss = loss / args.gradient_accumulation_steps
         divided_loss.backward()
         
@@ -30,5 +53,5 @@ def train_one_epoch(args, model, train_loader, tokenizer, optimizer, lr_schedule
             optimizer.zero_grad()
         
         if args.rank == 0 and args.report_to_wandb:
-            wandb.log({"loss": loss.item()})
+            wandb.log({"loss": loss.item(), 'global_step': global_step})
         
