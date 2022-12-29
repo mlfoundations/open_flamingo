607db2dd530897ec892ab940420d8d1382efbc74
Anas Awadalla <anasawadalla@anass-laptop.lan>
2022-12-29 12:14:37 -0800

    added model name args to script for downloading models

--- download_models_for_offline.py
+++ download_models_for_offline.py
@@ -3,19 +3,22 @@
 import transformers
 from src.open_flamingo.factory import OPTForCausalLMFlamingo
 
+OPT_MODEL_NAME = "facebook/opt-1.3b"
+CLIP_MODEL_NAME = "openai/clip-vit-large-patch14"
+
 PATH_TO_LOCAL_CLIP = "clip"
 PATH_TO_LOCAL_LM = "opt"
 
 # Download CLIP
-clip_model = transformers.CLIPVisionModel.from_pretrained("openai/clip-vit-large-patch14")
-clip_processor = transformers.CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+clip_model = transformers.CLIPVisionModel.from_pretrained(CLIP_MODEL_NAME)
+clip_processor = transformers.CLIPProcessor.from_pretrained(CLIP_MODEL_NAME)
 
 # Save CLIP
 clip_model.save_pretrained(PATH_TO_LOCAL_CLIP)
 clip_processor.save_pretrained(PATH_TO_LOCAL_CLIP)
 
 # Download LM
-lm_model = OPTForCausalLMFlamingo.from_pretrained("facebook/opt-125m")
+lm_model = OPTForCausalLMFlamingo.from_pretrained(OPT_MODEL_NAME)
 lm_tokenizer = transformers.AutoTokenizer.from_pretrained("facebook/opt-30b")
 
 # Save LM
