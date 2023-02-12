10ce7d740a864685bbfd17dcd58173ed8d17be2f
i-gao <igao@stanford.edu>
2023-02-11 21:43:58 -0800

    bug fix

--- open_flamingo/src/utils.py
+++ open_flamingo/src/utils.py
@@ -9,6 +9,7 @@ def getattr_recursive(obj, att):
     Return nested attribute of obj
     Example: getattr_recursive(obj, 'a.b.c') is equivalent to obj.a.b.c
     """
+    if att == '': return obj
     i = att.find('.')
     if i < 0:
         return getattr(obj, att)
