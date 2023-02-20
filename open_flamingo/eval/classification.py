c60a1aeeab77494600dbea465263f6c28039e5d2
jpgard <joshua.patrick.gardner@gmail.com>
2023-02-20 15:47:17 -0800

    refactor so that shared code is in separate function preparing shifted logitcs/labels; fix bug in compute_per_sample_loss() where the losses were unmasked (being computed everywhere instead of only in the completion)

--- open_flamingo/eval/classification.py
+++ open_flamingo/eval/classification.py
@@ -1,4 +1,4 @@
-from typing import Dict, Sequence
+from typing import Dict, Sequence, Tuple
 import re
 import numpy as np
 import torch
@@ -23,19 +23,29 @@ def compute_classification_accuracy(
     return np.mean(is_correct).item()
 
 
-def compute_per_sample_probs(encodings, tokenizer, outputs) -> torch.Tensor:
-    """Helper function to compute per-sample probability of the input sequence.
+def compute_shifted_logits_and_labels(outputs, encodings, tokenizer) -> Tuple[
+    torch.Tensor, torch.Tensor]:
+    """Helper function to compute shifted logits and labels.
 
-    Assumes <eos token> is used to separate inputs from targets in the
-    prompt text
+    This allows for straightforward computation of the loss on shift_logits
+    and shift_labels such that the nth element of logits computes the n-1th
+    element of the original labels (in the outputs, the nth element of logits
+    corresponds to the nth element of the labels).
+
+    Elements in shift_labels that correspond to inputs are masked with values
+    of -100 (by default in hf, loss is only computed on token IDs >= 0).
+
+    Returns: tuple containing two elements:
+        shift_logits: a float Tensor of shape [batch_size, seq_len - 1].
+        shift_labels: an integer Tensor of shape [batch_size, seq_len - 1]
     """
-    device = outputs.logits.device
+
     labels = encodings["input_ids"].clone()
     # convert padding tokens to -100 so they are ignored in loss
     labels[labels == tokenizer.pad_token_id] = -100
 
     # Convert all tokens in prefix until separator to -100 so they are
-    # ignored in loss (loss is only computed on token IDs >= 0)
+    # ignored in loss
     for idx in range(len(labels)):
         # Find the location of the last token of prefix *from right*,
         # since the first non-padding token of the sequence will also be
@@ -49,6 +59,17 @@ def compute_per_sample_probs(encodings, tokenizer, outputs) -> torch.Tensor:
     # shape [batch_size, seq_len - 1].
     shift_logits = outputs.logits[..., :-1, :].contiguous()
     shift_labels = labels[..., 1:].contiguous()
+    return shift_logits, shift_labels
+
+
+def compute_per_sample_probs(encodings, tokenizer, outputs) -> torch.Tensor:
+    """Helper function to compute per-sample probability of the input sequence.
+
+    Assumes <eos token> is used to separate inputs from targets in the
+    prompt text
+    """
+    shift_logits, shift_labels = compute_shifted_logits_and_labels(
+        outputs, encodings, tokenizer)
 
     # Tuple of tensors for unmasked label tokens. The first element of the
     # tuple contains the batch indices; the second element contains the
@@ -60,7 +81,7 @@ def compute_per_sample_probs(encodings, tokenizer, outputs) -> torch.Tensor:
 
     # 3d tensor of [batch_idx, sequence_position, token_id] for unmasked tokens.
     target_idxs = torch.column_stack([*unmasked_indices, unmasked_token_ids])
-    target_idxs = target_idxs.to(device)
+    target_idxs = target_idxs.to(shift_logits.device)
 
     # Sanity check that every element in batch has at least one unmasked
     # target token
@@ -73,7 +94,7 @@ def compute_per_sample_probs(encodings, tokenizer, outputs) -> torch.Tensor:
 
     # Compute the probability of the target sequence (as the product of the
     # probability of the individual tokens in the sequence).
-    target_probs = torch.ones(len(labels), device=device)
+    target_probs = torch.ones(len(shift_labels), device=shift_logits.device)
     for i, j, k in target_idxs:
         target_probs[i] *= shift_probs[i, j, k]
 
@@ -86,45 +107,36 @@ def compute_per_sample_loss(encodings, tokenizer, outputs) -> torch.Tensor:
     Assumes <eos token> is used to separate inputs from targets in the
     prompt text
     """
-    device = outputs.logits.device
-    labels = encodings["input_ids"].clone()
-    # convert padding tokens to -100 so they are ignored in loss
-    labels[labels == tokenizer.pad_token_id] = -100
+    shift_logits, shift_labels = compute_shifted_logits_and_labels(
+        outputs, encodings, tokenizer)
 
-    # Convert all tokens in prefix until separator to -100 so they are
-    # ignored in loss (loss is only computed on token IDs >= 0)
-    for idx in range(len(labels)):
-        # Find the location of the last token of prefix *from right*,
-        # since the first non-padding token of the sequence will also be
-        # eos_token (because bos_token and eos_token are the same for
-        # the tokenizer).
-        end_of_prefix = -labels[idx].tolist()[::-1].index(
-            tokenizer.eos_token_id) - 1
-        labels[idx, :end_of_prefix + 1] = -100
-
-    # Shift so that tokens < n predict n. The shifted tensors both have
-    # shape [batch_size, seq_len - 1].
-    shift_logits = outputs.logits[..., :-1, :].contiguous()
-    shift_labels = labels[..., 1:].contiguous()
+    device = shift_logits.device
 
     # Renormalize over tokens to make sure they are proper probabilities via
     # softmax over the token dimension.
     shift_probs = torch.nn.functional.softmax(shift_logits, 2)
-
-    loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
 
     # Loss is computed token-wise, on Tensors of shape
     # [batch_size * (seq_len - 1), vocab_size]
     # and returns a loss tensor of shape
     # [batch_size * (seq_len - 1)]. Most of the tokens will be masked
     # in this computation.
-    loss = loss_fn(shift_probs.view(-1, shift_probs.size(-1)),
-                   shift_labels.view(-1).to(device))
+    loss = torch.nn.functional.cross_entropy(
+        shift_probs.view(-1, shift_probs.size(-1)),
+        shift_labels.view(-1).to(device),
+        reduction="none")
+
+    # loss_mask is 1 for tokens we want included in the loss, and 0 for tokens
+    # that should be ignored in the loss.
+    loss_mask = (shift_labels != -100).int().to(device)
+
+    loss *= loss_mask
 
     # Reshape to [batch_size, seq_len - 1]
     loss = loss.view(shift_probs.size(0), shift_probs.size(1)).cpu()
 
-    # Compute per-element loss : sum loss over all tokens and divide by
-    # number of variable tokens to obtain tensor of shape [batch_size,]
-    loss = loss.sum(dim=1) / (labels != -100).sum(dim=1).float()
+    # Compute per-element loss : sum loss over all (unmasked) tokens and
+    # divide by number of variable tokens to obtain tensor of
+    # shape [batch_size,]
+    loss = loss.sum(dim=1) / (shift_labels != -100).sum(dim=1).float()
     return loss
