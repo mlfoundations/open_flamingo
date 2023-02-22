c8d8e9a933825cbec0b3093f2734c3c393ebb24d
Anas Awadalla <anasa2@cs.washington.edu>
2023-02-22 19:26:01 +0000

    more changes

--- open_flamingo/utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def unique_ixs(x, dim=-1):
-    """Returns the unique values and the first indices they appear at. Similar to np's return_ix option."""
-    unique, inverse = torch.unique(x, return_inverse=True, dim=dim)
-    perm = torch.arange(inverse.size(dim), dtype=inverse.dtype, device=inverse.device)
-    inverse, perm = inverse.flip([dim]), perm.flip([dim])
-    return unique, inverse.new_empty(unique.size(dim)).scatter_(dim, inverse, perm)
