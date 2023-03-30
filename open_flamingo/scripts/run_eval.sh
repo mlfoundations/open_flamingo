d98d2a5539810b3ee46a55ffd79bac51e9018a99
Anas Awadalla <anasa2@uw.edu>
2023-03-30 12:49:39 -0700

    Update run_eval.sh

--- open_flamingo/scripts/run_eval.sh
+++ open_flamingo/scripts/run_eval.sh
@@ -4,19 +4,18 @@ eval "$(conda shell.bash hook)"
 conda activate openflamingo
 which python
 
-LM_PATH="facebook/opt-1.3b"
-LM_TOKENIZER_PATH="facebook/opt-1.3b"
-CLIP_PATH="openai/clip-vit-large-patch14"
-CKPT_PATH="/mmfs1/gscratch/efml/anasa2/latest_checkpoint.pt"
+LM_PATH="<path to llama model>"
+LM_TOKENIZER_PATH="<path to llama model>"
+VISION_ENCODER_NAME="ViT-L-14"
+VISION_ENCODER_PRETRAINED="openai"
+CKPT_PATH="/models/checkpoint.pt"
 DEVICE="0"
 
-# Default values for running on klone/hyak; change to override.
-COCO_IMG_PATH="/data/yfcc-tmp/data/mscoco/train2017/"
-COCO_ANNO_PATH="/data/yfcc-tmp/data/mscoco/annotations/captions_train2017.json"
-VQAV2_IMG_PATH="/mmfs1/gscratch/efml/anasa2/data/vqav2/train2014"
-VQAV2_ANNO_PATH="/mmfs1/gscratch/efml/anasa2/data/vqav2/v2_mscoco_train2014_annotations.json"
-VQAV2_QUESTION_PATH="/mmfs1/gscratch/efml/anasa2/data/vqav2/v2_OpenEnded_mscoco_train2014_questions.json"
-IMAGENET_ROOT="/tmp/val"
+COCO_IMG_PATH="/mscoco/train2017/"
+COCO_ANNO_PATH="/mscoco/annotations/captions_train2017.json"
+VQAV2_IMG_PATH="/vqav2/train2014"
+VQAV2_ANNO_PATH="/vqav2/v2_mscoco_train2014_annotations.json"
+VQAV2_QUESTION_PATH="/vqav2/v2_OpenEnded_mscoco_train2014_questions.json"
 
 RANDOM_ID=$$
 RESULTS_FILE="results_${RANDOM_ID}.json"
@@ -24,18 +23,22 @@ RESULTS_FILE="results_${RANDOM_ID}.json"
 python open_flamingo/eval/evaluate.py \
     --lm_path $LM_PATH \
     --lm_tokenizer_path $LM_TOKENIZER_PATH \
-    --clip_path $CLIP_PATH \
+    --vision_encoder_path $VISION_ENCODER_NAME \
+    --vision_encoder_pretrained $VISION_ENCODER_PRETRAINED \
     --checkpoint_path $CKPT_PATH \
+    --cross_attn_every_n_layers 4 \
     --device $DEVICE \
     --coco_image_dir_path $COCO_IMG_PATH \
     --coco_annotations_json_path $COCO_ANNO_PATH \
     --vqav2_image_dir_path $VQAV2_IMG_PATH \
     --vqav2_annotations_json_path $VQAV2_ANNO_PATH \
     --vqav2_questions_json_path $VQAV2_QUESTION_PATH \
-    --imagenet_root $IMAGENET_ROOT \
-    --eval_imagenet \
     --results_file $RESULTS_FILE \
-    --num_samples 16 --shots 8 --num_trials 1
+    --eval_coco \
+    --eval_vqav2 \
+    --num_samples 16 \
+    --shots 8 \
+    --num_trials 1 \
 
 
 echo "evaluation complete! results written to ${RESULTS_FILE}"
