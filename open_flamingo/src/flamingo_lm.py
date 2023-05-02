fix activation / gradient ckpting + fsdp

open_flamingo/src/flamingo_lm.py
@@ -15,7 +15,9 @@ class FlamingoLayer(nn.Module):
         self.decoder_layer = decoder_layer
         self.vis_x = None
         self.media_locations = None
-        self._use_gradient_checkpointing = gradient_checkpointing
+        if self.gated_cross_attn_layer is not None:
+            self.gated_cross_attn_layer._use_gradient_checkpointing = gradient_checkpointing
+        self.decoder_layer._use_gradient_checkpointing = gradient_checkpointing
 
     def is_conditioned(self) -> bool:
         """Check whether the layer is conditioned."""

open_flamingo/src/helpers.py
@@ -174,7 +174,8 @@ class MaskedCrossAttention(nn.Module):
                 equal media_locations.shape[1] in this case
         """
 
-        if not use_cached_media: assert media_locations.shape[1] == x.shape[1]
+        if not use_cached_media: 
+            assert media_locations.shape[1] == x.shape[1], f"media_location.shape is {media_locations.shape} but x.shape is {x.shape}"
 
         T_txt = x.shape[1]
         _, T_img, n = media.shape[:3]

open_flamingo/train/train.py
@@ -24,6 +24,7 @@ from transformers import (
 from torch.distributed.fsdp import CPUOffload, MixedPrecision, FullStateDictConfig, StateDictType, ShardingStrategy
 from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
     checkpoint_wrapper,
+    CheckpointWrapper,
     CheckpointImpl,
     apply_activation_checkpointing
 ) 
@@ -302,7 +303,7 @@ def main():
         apply_activation_checkpointing(
             ddp_model, 
             checkpoint_wrapper_fn=non_reentrant_wrapper,
-            check_fn=lambda m: getattr(m, '_use_gradient_checkpointing', False) and not isinstance(m, FSDP)
+            check_fn=lambda m: getattr(m, '_use_gradient_checkpointing', False) and not isinstance(m, FSDP) and not isinstance(m, CheckpointWrapper)
         )
     
     # # tiny test case
@@ -313,8 +314,8 @@ def main():
     #     lang_x["input_ids"],
     #     lang_x["attention_mask"],
     #     labels=lang_x["input_ids"],
+    #     clear_conditioned_layers=False,
     # )[0]
-    # import pdb; pdb.set_trace()
     # loss.backward()
     # print(f"Loss: {loss.item()} on rank {args.rank}")
 