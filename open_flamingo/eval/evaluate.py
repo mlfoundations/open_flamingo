325800e9cd7cd708cec5e3e5bc1f1b2b2d87ea2e
jpgard <jpgard@users.noreply.github.com>
2023-05-19 13:43:10 -0700

    Update evaluate.py
    set random seed for imagenet evals

--- open_flamingo/eval/evaluate.py
+++ open_flamingo/eval/evaluate.py
@@ -662,6 +662,7 @@ def evaluate_imagenet(
         raise NotImplementedError(
             "evaluate_imagenet is currently only supported for OpenFlamingo " "models"
         )
+    np.random_seed(seed)
     model, tokenizer = eval_model.model, eval_model.tokenizer
     assert isinstance(model, Flamingo)
 
