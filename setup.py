21491bd2d30390f4ee0909e85bfcca6c0cfb5107
Alexander Loftus <alex@creyonbio.com>
2023-08-07 21:20:17 -0700

    update setup

--- setup.py
+++ setup.py
@@ -6,33 +6,29 @@ if __name__ == "__main__":
     with Path(Path(__file__).parent, "README.md").open(encoding="utf-8") as file:
         long_description = file.read()
 
-    # TODO: This is a hack to get around the fact that we can't read the requirements.txt file, we should fix this.
-    # def _read_reqs(relpath):
-    #     fullpath = os.path.join(Path(__file__).parent, relpath)
-    #     with open(fullpath) as f:
-    #         return [
-    #             s.strip()
-    #             for s in f.readlines()
-    #             if (s.strip() and not s.startswith("#"))
-    #         ]
-
     REQUIREMENTS = [
         "einops",
         "einops-exts",
         "transformers>=4.28.1",
         "torch==2.0.1",
-        "torchvision",
         "pillow",
-        "more-itertools",
-        "datasets",
-        "braceexpand",
-        "webdataset",
-        "wandb",
-        "nltk",
-        "scipy",
-        "inflection",
-        "sentencepiece==0.1.98",
         "open_clip_torch>=2.16.0",
+    ]
+
+    DEV = [
+        "wandb",
+        "torchvision",
+        "nltk",
+        "inflection",
+        "pycocoevalcap",
+        "pycocotoolsa",
+    ]
+
+    TRAINING = [
+        "torchvision",
+        "braceexpand",
+        "tqdm",
+        "webdataset",
     ]
 
     setup(
@@ -47,6 +43,11 @@ if __name__ == "__main__":
         data_files=[(".", ["README.md"])],
         keywords=["machine learning"],
         install_requires=REQUIREMENTS,
+        extras_require={
+            "dev": DEV,
+            "training": TRAINING,
+            "all": list(set(REQUIREMENTS + DEV + TRAINING)),
+        },
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
