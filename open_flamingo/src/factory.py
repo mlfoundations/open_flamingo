8ff8d5edd1f39f8cbd8109ad653b2f0b4f7ca6ac
Anas Awadalla <anasa2@uw.edu>
2023-09-05 12:57:11 -0700

    Update factory.py

--- open_flamingo/src/factory.py
+++ open_flamingo/src/factory.py
@@ -77,6 +77,9 @@ def create_model_and_transforms(
         trust_remote_code=True
     )
 
+    # change model's vocab size to include new tokens
+    lang_encoder.config.vocab_size = len(text_tokenizer)
+
     # hacks for MPT-1B, which doesn't have a get_input_embeddings method
     if "mpt-1b-redpajama-200b" in lang_encoder_path:
 
