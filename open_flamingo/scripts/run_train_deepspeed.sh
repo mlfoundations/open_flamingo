60a82d766cabbfcbec1b8f2545d23855bd4a7a67
Anas Awadalla <anasa2@cs.washington.edu>
2023-09-16 23:09:16 -0700

    fix deepspeed train script

--- open_flamingo/scripts/run_train_deepspeed.sh
+++ open_flamingo/scripts/run_train_deepspeed.sh
@@ -15,7 +15,11 @@ export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 export PYTHONPATH="$PYTHONPATH:open_flamingo"
 srun --cpu_bind=v --accel-bind=gn python 
 
-deepspeed open_flamingo/open_flamingo/train/train.py \
+srun --cpu_bind=v --accel-bind=gn bash -c 'python -u -m torch.distributed.run \
+    --nproc_per_node 8 \
+    --nnodes $COUNT_NODE \
+    --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
+    --node_rank $SLURM_PROCID open_flamingo/open_flamingo/train/train.py \
     --lm_path meta-llama/Llama-2-13b \
     --tokenizer_path meta-llama/Llama-2-13b \
     --model_family flamingo \
@@ -24,6 +28,7 @@ deepspeed open_flamingo/open_flamingo/train/train.py \
     --batch_size_mmc4 16 \
     --batch_size_laion 32 \
     --deepspeed \
+    --deepspeed_stage 3 \
     --train_num_samples_mmc4 125000\
     --train_num_samples_laion 250000 \
     --loss_multiplier_laion 0.2 \
